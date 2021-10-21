791c233084f65356b7e2c452cdeb3aa367d2cf8f
hotbreakb <64337152+hotbreakb@users.noreply.github.com>
2021-10-21 19:06:04 +0900

    css: input을 클릭할 때마다 버튼이 커지는 현상 해결
    btn의 width가 지정되지 않으면 알아서 움직임

--- 02-COUNTER/app.js
+++ 02-COUNTER/app.js
@@ -1,1 +1,47 @@
 const count = document.querySelector(".count");
+const btns = document.querySelectorAll("button");
+const unit = document.querySelector(".unit");
+
+let value = 0;
+
+btns.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+        let numberUnit = parseInt(unit.value, 10);
+
+        // if unit is black
+        if (isNaN(numberUnit)) {
+            numberUnit = 1;
+        }
+
+        // calcurate
+        if (e.currentTarget.classList.contains("decrease")) {
+            value -= numberUnit;
+        }
+        else if (e.currentTarget.classList.contains("increase")) {
+            value += numberUnit;
+        }
+        else {
+            value = 0;
+        }
+
+        // change font color by value
+        if (value < 0) {
+            count.style.color = 'red';
+        }
+        else if (value > 0) {
+            count.style.color = 'green';
+        }
+        else {
+            count.style.color = 'black';
+        }
+
+        count.textContent = value;
+    });
+});
+
+function inputNumberLengthOne(e) {
+
+    if (e.value.length > 0) {
+        e.value = e.value.replace(/[^0-9.]/g, '').replace(/(\..*)\./g, '');
+    }
+}
